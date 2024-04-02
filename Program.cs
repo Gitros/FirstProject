95ab8d6fd2b66181d6b6de9f68c0ce91870e3d7e
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-02 11:46:44 +0200

    clear

--- Program.cs
+++ Program.cs
@@ -1,11 +1,1 @@
-﻿/*
-int saleAmount = 1001;
-int discountValue = saleAmount > 1000 ? 100 : 50;
-Console.WriteLine($"discount: {discountValue}");
-*/
-
-int saleAmount = 1001;
-
-Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
-
-
+﻿
