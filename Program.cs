b0440f96c9fbe4b626ea57d9eb574bd66697909f
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-02 11:46:00 +0200

    conditional statements

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿/*
+int saleAmount = 1001;
+int discountValue = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"discount: {discountValue}");
+*/
+
+int saleAmount = 1001;
+
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+
