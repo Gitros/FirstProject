b9f6f36b372688e1b36918cafe4e2bdc6f3ebfbf
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-02 12:38:18 +0200

    permission exercise

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string permission = "Manager";
+int level = 15;
+
+if (level > 55 && permission.Contains("Admin"))
+{
+    Console.WriteLine("Welcome, super admin user");
+}
+else if (level >= 55 && permission.Contains("Admin"))
+{
+    Console.WriteLine("Welcome, Admin user.");
+}
+else if (level >= 20 && permission.Contains("Manager"))
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else if (level < 20 && permission.Contains("Manager"))
+{
+    Console.WriteLine("You dont have sufficient privileges.");
+}
+else if (!permission.Contains("Admin") || !permission.Contains("Manager"))
+{
+    Console.WriteLine("You dont have sufficient privileges.");
+}
