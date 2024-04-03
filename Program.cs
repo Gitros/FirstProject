e4eb31e665fa450e47db7c29b3fe54017c71541d
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-03 16:57:18 +0200

    roll dice game

--- Program.cs
+++ Program.cs
@@ -6,4 +6,29 @@ int roll3 = dice.Next(1, 7);
 
 int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+string extraCalculation = "";
+
+if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+{
+    total += 2;
+    extraCalculation = "+ 2";
+}
+
+if (roll1 == roll2 && roll1 == roll3 && roll2 == roll3)
+{
+    total += 6;
+    extraCalculation = "+ 6";
+}
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} {extraCalculation} = {total}");
+if (total >= 15)
+{
+    Console.WriteLine("You won!");
+}
+else
+{
+    Console.WriteLine("You lost.");
+}
+
+
+
