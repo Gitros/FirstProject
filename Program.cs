fc91a162a9faadd16f202972acb8b2b49cb1805b
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-06 12:36:33 +0200

    values

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿bool flag = true;
+int value = 0;
+if (flag)
+{
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
