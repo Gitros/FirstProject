75069ec457a501b379c329371a00c2794a9b1f36
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-08 11:01:36 +0200

    switch case

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿switch (fruit)
+{
+    case "apple":
+        Console.WriteLine($"App will display information for apple.");
+        break;
+
+    case "banana":
+        Console.WriteLine($"App will display information for banana.");
+        break;
+
+    case "cherry":
+        Console.WriteLine($"App will display information for cherry.");
+        break;
+}
